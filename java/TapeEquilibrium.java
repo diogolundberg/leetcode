fd7a67afeb1d1ebcd38b73869c9be9ca1de1b395
Diogo Lundberg <dclundberg@gmail.com>
2019-10-13 17:59:55 +0100

    Lesson 3: Time Complexity - Tape equilibrium

---
+++ java/TapeEquilibrium.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TapeEquilibrium {
+    public int solution(int[] A) {
+        int left = A[0];
+        int right = 0;
+        for (int i = 1; i < A.length; i++) {
+            right += A[i];
+        }
+        int difference = Math.abs(left - right);
+
+        for(int i = 1; i < A.length - 1; i++) {
+            left += A[i];
+            right -= A[i];
+
+            int newDifference = Math.abs(left - right);
+            difference = newDifference < difference ? newDifference : difference;
+        }
+
+        return difference;
+    }
+
+    @Test
+    public void test(){
+        TapeEquilibrium solution = new TapeEquilibrium();
+        assertEquals(2000, solution.solution(new int[]{-1000, 1000}));
+        assertEquals(1, solution.solution(new int[]{3, 1, 2, 4, 3}));
+    }
+}
---
+++ java/TapeEquilibriumStream.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TapeEquilibriumStream {
+    public int solution(int[] A) {
+        int left = A[0];
+        int right = Arrays.stream(A).skip(1).sum();
+        int difference = Math.abs(left - right);
+
+        for(int i = 1; i < A.length - 1; i++) {
+            left += A[i];
+            right -= A[i];
+
+            int newDifference = Math.abs(left - right);
+            difference = newDifference < difference ? newDifference : difference;
+        }
+
+        return difference;
+    }
+
+    @Test
+    public void test(){
+        TapeEquilibriumStream solution = new TapeEquilibriumStream();
+        assertEquals(2000, solution.solution(new int[]{-1000, 1000}));
+        assertEquals(1, solution.solution(new int[]{3, 1, 2, 4, 3}));
+    }
+}
