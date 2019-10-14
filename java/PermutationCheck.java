c946a7bb25ae5a745c081e0ce4d76f3679df00b1
Diogo Lundberg <dclundberg@gmail.com>
2019-10-14 10:56:29 +0100

    Lesson 4: Counting Elements - Permutation check

---
+++ java/PermutationCheck.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PermutationCheck {
+    public int solution(int[] A) {
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            result ^= A[i] ^ (i + 1);
+        }
+
+        return result == 0 ? 1 : 0;
+    }
+
+    @Test
+    public void test(){
+        PermutationCheck solution = new PermutationCheck();
+        assertEquals(3, solution.solution(new int[]{1, 2, 4}));
+    }
+}
