9d1973c170ee1e2634b52328f098ede7ef812513
Diogo Lundberg <dclundberg@gmail.com>
2025-09-29 20:32:23 +0200

    Add type hints to odd_occurrences_in_array

--- python/odd_occurrences_in_array.py
+++ python/odd_occurrences_in_array.py
@@ -1,6 +1,9 @@
-def solution(A):
-    count = dict()
+def solution(A: list[int]) -> int:
+    count: dict[int, int] = {}
     for number in A:
-        occurrences = count.get(number, 0)
+        occurrences: int = count.get(number, 0)
         count[number] = occurrences + 1
-    return [key for key, value in count.items() if value % 2 > 0][0]
+    for key, value in count.items():
+        if value % 2 == 1:
+            return key
+    raise ValueError("No element with odd occurrences found")
