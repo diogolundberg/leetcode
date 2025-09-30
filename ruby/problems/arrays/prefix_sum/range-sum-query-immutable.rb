6f7c6bd1a49e87c6a20b902ab219f3d090857dce
Diogo Lundberg <dclundberg@gmail.com>
2025-09-30 17:52:57 +0200

    Add solution for Range query sum immutable

--- ruby/problems/arrays/prefix_sum/range-sum-query-immutable.rb
+++ ruby/problems/arrays/prefix_sum/range-sum-query-immutable.rb
@@ -1,37 +1,32 @@
 require "minitest/autorun"
 
 =begin
-Given an integer array nums, handle multiple queries of the following types:
-1. Update the value of an element in nums.
-2. Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Given an integer array nums, handle multiple queries of the following type:
+1. Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
 
 Implement the NumArray class:
 - NumArray(int[] nums) Initializes the object with the integer array nums.
-- void update(int index, int val) Updates the value of nums[index] to be val.
 - int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
-
 
 Example 1:
 Input
-  ["NumArray", "sumRange", "update", "sumRange"]
-  [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+  ["NumArray", "sumRange", "sumRange", "sumRange"]
+  [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
 Output
-  [null, 9, null, 8]
+  [null, 1, -1, -3]
 
 Explanation
-  NumArray numArray = new NumArray([1, 3, 5]);
-  numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
-  numArray.update(1, 2);   // nums = [1, 2, 5]
-  numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
+  NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+  numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+  numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+  numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 
 
 Constraints:
-- 1 <= nums.length <= 3 * 104
-- -100 <= nums[i] <= 100
-- 0 <= index < nums.length
-- -100 <= val <= 100
+- 1 <= nums.length <= 104
+- -105 <= nums[i] <= 105
 - 0 <= left <= right < nums.length
-- At most 3 * 104 calls will be made to update and sumRange.
+- At most 104 calls will be made to sumRange.
 =end
 
 class NumArray
@@ -40,19 +35,23 @@ class NumArray
     :type nums: Integer[]
 =end
     def initialize(nums)
+      @nums = nums
 
+      # Build a cumulative sum array with one extra slot.
+      # @prefix_sum[i] = sum of nums[0...i] (all elements before index i).
+      #
+      # Why +1 size?
+      # This way, prefix_sum[0] = 0 (empty sum),
+      # and prefix_sum[n] = sum of all nums (so right+1 is always safe).
+      @prefix_sum = Array.new(nums.length + 1, 0)
+
+      # Fill cumulative totals:
+      # prefix_sum[i+1] = prefix_sum[i] + nums[i]
+      # → "running balance": each new slot adds one more number.
+      (0...nums.length).each do |i|
+        @prefix_sum[i + 1] = @prefix_sum[i] + nums[i]
+      end
     end
-
-
-=begin
-    :type index: Integer
-    :type val: Integer
-    :rtype: Void
-=end
-    def update(index, val)
-
-    end
-
 
 =begin
     :type left: Integer
@@ -60,22 +59,27 @@ class NumArray
     :rtype: Integer
 =end
     def sum_range(left, right)
-
+      # Math theory:
+      # prefix_sum[right+1] = sum of nums[0..right]
+      # prefix_sum[left]    = sum of nums[0..left-1]
+      #
+      # Subtraction cancels everything before "left":
+      # (nums[0]..nums[right]) - (nums[0]..nums[left-1])
+      # = nums[left]..nums[right]
+      @prefix_sum[right+1] - @prefix_sum[left]
     end
-
-
 end
 
 class TestNumArray < Minitest::Test
   def test_example
-    num_array = NumArray.new([1, 3, 5])
-    assert_equal 9, num_array.sum_range(0, 2)
+    num_array = NumArray.new([-2, 0, 3, -5, 2, -1])
 
-    num_array.update(1, 2)
-    assert_equal 8, num_array.sum_range(0, 2)
+    assert_equal 1, num_array.sum_range(0, 2)
+    assert_equal -1, num_array.sum_range(2, 5)
+    assert_equal -3, num_array.sum_range(0, 5)
   end
 end
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray.new(nums)
-# obj.update(index, val)
-# param_2 = obj.sum_range(left, right)
+# param_1 = obj.sum_range(left, right)
