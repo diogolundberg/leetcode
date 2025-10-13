6608a05e08f45c244542c168fef6070365ad22f4
Diogo Lundberg <dclundberg@gmail.com>
2025-10-13 21:57:41 +0200

    Add rubocop.yml

--- ruby/problems/arrays/prefix_sum/range_sum_query_immutable.rb
+++ ruby/problems/arrays/prefix_sum/range_sum_query_immutable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'minitest/autorun'
 
 # Given an integer array nums, handle multiple queries of the following type:
@@ -7,7 +5,8 @@ require 'minitest/autorun'
 #
 # Implement the NumArray class:
 # - NumArray(int[] nums) Initializes the object with the integer array nums.
-# - int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+# - int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive
+#  (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 #
 # Example 1:
 # Input
