95ced7814a23bbdc21e648be05252e96c5cd3f56
Diogo Lundberg <dclundberg@gmail.com>
2025-10-28 22:35:24 +0100

    Add service object pattern

---
+++ ruby/design_patterns/service_object.rb
@@ -0,0 +1,72 @@
+require 'minitest/autorun'
+
+class Result
+  attr_reader :value
+
+  class Success < Result; end
+  class Failure < Result; end
+
+  def initialize(value)
+    @value = value
+  end
+
+  def self.success(value)
+    Success.new(value)
+  end
+
+  def self.failure(error)
+    Failure.new(error)
+  end
+end
+
+class UseCase
+  def initialize(repository)
+    @repository = repository
+  end
+
+  def call(params)
+    @repository.save(params)
+    Result.success('output')
+  rescue StandardError => e
+    Result.failure(e)
+  end
+end
+
+class ReconcileFileUseCaseTest < Minitest::Test
+  def setup
+    @repository = Minitest::Mock.new
+    @usecase = UseCase.new(@repository)
+  end
+
+  def test_should_return_success
+    expected_params = { id: '123', total: 100, amount: 50 }
+    @repository.expect(:save, true, [expected_params])
+
+    result = @usecase.call(expected_params)
+
+    assert_instance_of(Result::Success, result)
+    assert_equal('output', result.value)
+  end
+
+  def test_should_return_failure
+    expected_params = { id: '123', total: 100, amount: 50 }
+
+    @repository.expect(:save, nil, [expected_params])
+    def @repository.save(_) = raise(StandardError, 'DB is down')
+
+    result = @usecase.call(expected_params)
+
+    assert_instance_of(Result::Failure, result)
+    assert_instance_of(StandardError, result.value)
+    assert_equal('DB is down', result.value.message)
+  end
+
+  def test_call_repository_save
+    expected_params = { id: '123', total: 100, amount: 50 }
+    @repository.expect(:save, true, [expected_params])
+
+    @usecase.call(expected_params)
+
+    @repository.verify
+  end
+end
